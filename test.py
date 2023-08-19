2efd094ce66cb9dad0af9a00ffb9bcf59072c4f0
malizudc <malizu.cdf@gmail.com>
2023-08-19 11:55:25 +0100

    new strings

--- test.py
+++ test.py
@@ -12,3 +12,15 @@ jname= "JAMES"
 mgreet= "GOOD AFTERNOON"
 mgreeting= mgreet + " "+jname + ", " +"HOW ARE YOU"
 print (mgreeting)
+
+str="HOW ARE YOU?"
+str_len= len(str)
+print (str_len)
+
+str1= "PETER IS A GOOD BOY"
+print (str1[11:15])
+str1_len= len(str1)
+print (str1_len)
+
+strr="j" + [1:2]
+print (strr)
