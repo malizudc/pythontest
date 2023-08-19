4bf1aa4b31499da5d84fde5b70ea153d047fef5e
malizudc <malizu.cdf@gmail.com>
2023-08-19 10:56:13 +0100

    python test file

---
+++ test.py
@@ -0,0 +1,14 @@
+message= 2
+print (message)
+name= 'JOHN'
+mymessage= f'HI {name}'
+print (mymessage)
+
+myname= "PETER"
+greeting= f'GOOD MORNING {myname}'
+print (greeting)
+
+jname= "JAMES"
+mgreet= "GOOD AFTERNOON"
+mgreeting= mgreet + " "+jname + ", " +"HOW ARE YOU"
+print (mgreeting)
